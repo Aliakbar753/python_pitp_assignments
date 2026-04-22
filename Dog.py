aca133372ef2ff86f5f4f11b4d4c1d350b7d8598
Aliakbar753 <131573293+Aliakbar753@users.noreply.github.com>
2026-04-22 13:25:47 +0500

    Add files via upload

---
+++ Dog.py
@@ -0,0 +1,22 @@
+class Dog:
+
+    name = 'tommy'
+    color = 'black'
+
+    def eating(dog):
+        print('Dog can eat')
+
+    def barking(dog):
+        print("dog can bark ")
+
+    def sleap(dog):
+        print("dog can sleep")
+
+dog = Dog()
+
+# print(dog.name)
+# print(dog.color)
+
+dog.eating()
+dog.barking()
+dog.sleap() 
---
+++ Man.py
@@ -0,0 +1,10 @@
+class Man:
+    name = 'ali'
+    age = 20
+
+    def eat():
+        print('eat')
+
+man = Man()
+print(man.name)
+print(man.age)
